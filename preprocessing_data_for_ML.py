bbf582a90c0aaddf955336772b55730865832595
samuel787 <suther.david.samuel@gmail.com>
2020-07-31 21:21:31 +0800

    ML prediction model

--- preprocessing_data_for_ML.py
+++ preprocessing_data_for_ML.py
@@ -1,6 +1,11 @@
+from collections import Counter
 import numpy as np
 import pandas as pd
 import pickle
+from sklearn import svm, neighbors
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 def process_data_for_labels(ticker):
     hm_days = 7
@@ -14,13 +19,87 @@ def process_data_for_labels(ticker):
     df.fillna(0, inplace = True)
     return tickers, df
 
+#process_data_for_labels('AES')
+
 def buy_sell_hold(*args):
     cols = [c for c in args]
-    requirement = 0.02 #threshold
+    requirement = 0.02 #threshold -> tweak this number so that it is even btw buy, sell, hold
     for col in cols:
         if col > requirement:
             return 1
         if col < -requirement:
             return -1
     return 0
-#process_data_for_labels('AES')
+
+### extract feature sets and labels
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+    df['{}_target'.format(ticker)] = list(map(
+        buy_sell_hold,
+        df['{}_1d'.format(ticker)],
+        df['{}_2d'.format(ticker)],
+        df['{}_3d'.format(ticker)],
+        df['{}_4d'.format(ticker)],
+        df['{}_5d'.format(ticker)],
+        df['{}_6d'.format(ticker)],
+        df['{}_7d'.format(ticker)]
+    ))
+
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print("Data spread:", Counter(str_vals))
+    
+    df.fillna(0, inplace = True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace = True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace = True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+def do_ml_kneighbour_classifier(ticker):
+    X, y, df = extract_featuresets(ticker)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size = 0.25
+    )
+
+    clf = neighbors.KNeighborsClassifier()
+    
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print("Accuracy", confidence)
+    predictions = clf.predict(X_test)
+    print("Predicted spread: ", Counter(predictions))
+
+    return confidence
+
+do_ml_kneighbour_classifier('AES')
+
+def do_ml_voting_classifier(ticker):
+    X, y, df = extract_featuresets(ticker)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size = 0.25
+    )
+
+    clf = VotingClassifier(
+        [
+            ('lsvc', svm.LinearSVC()),
+            ('knn', neighbors.KNeighborsClassifier()),
+            ('rfor', RandomForestClassifier())
+        ]
+    )
+    
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print("Accuracy", confidence)
+    predictions = clf.predict(X_test)
+    print("Predicted spread: ", Counter(predictions))
+
+    return confidence
+
+do_ml_voting_classifier('ABT')
